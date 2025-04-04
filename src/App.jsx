b352ba106cc120afaaeda38a5673b2a6c73a097f
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-04-04 11:14:38 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='bg-black text-6xl rounded-2xl p-4 text-green-400 w-full'>EntryDesh</h1>
+    </>
+  )
+}
+
+export default App
