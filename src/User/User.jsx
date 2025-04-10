fc2934d3608ef47c7037768c61fb9ee6a9d37300
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-04-10 15:40:06 +0530

    .

--- src/User/User.jsx
+++ src/User/User.jsx
@@ -3,7 +3,10 @@ import { useParams } from 'react-router-dom'
 function User() {
     const {userId} = useParams()
   return (
-    <div className='p-4 bg-black text-green-400 w-full text-3xl rounded-2xl mt-10 mb-10'>User: {userId} </div>
+<>
+<div className='p-4 bg-black text-green-400 w-full text-3xl rounded-2xl mt-10 mb-10'>User: {userId} </div>
+
+</>
   )
 }
 
